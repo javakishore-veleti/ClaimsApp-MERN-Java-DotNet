a86cc3e4c49487ea87fa118dbd5ebc1b22938ff8
Aruna Krishna <akrishna@awarenesstech.com>
2023-05-21 08:47:11 -0400

    Initial Commit

---
+++ backend/tickets-processing-app/src/main/java/org/jk/modernization/learnings/App.java
@@ -0,0 +1,13 @@
+package org.jk.modernization.learnings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
